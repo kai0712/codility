d2c7b386456d293055d76890358cb830d45744b6
Yaojia Wang <yaojia.wang@playngo.com>
2016-11-28 12:23:22 +0100

    Init project and init test project.

---
+++ Codility/Codility.Tests/BinaryGapTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using Codility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Tests
+{
+	public class BinaryGapTests
+	{
+		[Fact()]
+		public void GetBinaryGapTest()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Codility/Codility/BinaryGap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility
+{
+	public static class BinaryGap
+	{
+		public static int GetBinaryGap(int value)
+		{
+			return 0;
+		}
+	}
+}
